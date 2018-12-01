af3435885647456b3be002218ddc2e77391da7c5
StefanoRizzi <39398961+StefanoRizzi@users.noreply.github.com>
2018-12-01 17:54:24 +0100

    Add files via upload
    Novità: aggiunto checkBox dell'address e le chat private (in fase di testing)

---
+++ aggiornati 1.1/JServer/src/jserver/MainInterface.java
@@ -0,0 +1,304 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jserver;
+
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author Stefano
+ */
+public class MainInterface extends javax.swing.JFrame{
+
+    private MyServer server;
+    
+    /**
+     * Creates new form MainInterface
+     */
+    public MainInterface(MyServer server) {
+        this.server= server;
+        initComponents();
+        init();
+    }
+    
+    private void init(){
+        banButton.setEnabled(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public void addMessage(String[] message){
+        String[] wClient;
+        String writer, recipient, addrWriter= "", time, text;
+        writer= message[0];
+        recipient= message[1];
+        time= message[2];
+        text= message[3];
+        wClient= server.getClientByName(writer);
+        if(wClient != null) addrWriter= wClient[1];
+        chatTextPane.setText(chatTextPane.getText() /*+ time*/ + "[" + recipient + "] " + writer + addrWriter + ": " + text + "\n");
+    }
+    
+    public void reloadClients(){
+        DefaultListModel model= new DefaultListModel();
+        String[][] clients= server.getClients();
+        String hostname, state;
+        for(String[] client : clients){
+            hostname= client[0];
+            state= client[2];
+            if(state.equals(MyServer.S_Ban)) state= "(bannato)";
+            else if(state.equals(MyServer.S_Dis)) state= "(offline)";
+            else state= "";
+            model.addElement(hostname + " " + state);
+        }
+        clientsList.setModel(model);
+    }
+    
+    private void reloadMessages(){
+        String[][] messaggi= server.getMessaggi();
+        chatTextPane.setText("");
+        for(String[] message : messaggi){
+            addMessage(message);
+        }
+    }
+    
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        mainPanel = new javax.swing.JPanel();
+        clientsPanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        clientsList = new javax.swing.JList<>();
+        banButton = new javax.swing.JButton();
+        chatPanel = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        chatTextPane = new javax.swing.JTextPane();
+        logLabel = new javax.swing.JLabel();
+        messageTextField = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        sendButton = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        impostazioniMenu = new javax.swing.JMenu();
+        addrCheckBox = new javax.swing.JCheckBoxMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Server");
+        setResizable(false);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel1.setText("Clients:");
+
+        clientsList.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        clientsList.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                clientsListValueChanged(evt);
+            }
+        });
+        jScrollPane1.setViewportView(clientsList);
+
+        banButton.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        banButton.setText("BANNING");
+        banButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                banButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout clientsPanelLayout = new javax.swing.GroupLayout(clientsPanel);
+        clientsPanel.setLayout(clientsPanelLayout);
+        clientsPanelLayout.setHorizontalGroup(
+            clientsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(clientsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(clientsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(clientsPanelLayout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addComponent(banButton, javax.swing.GroupLayout.DEFAULT_SIZE, 185, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        clientsPanelLayout.setVerticalGroup(
+            clientsPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(clientsPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(banButton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel2.setText("Global Chat");
+
+        chatTextPane.setEditable(false);
+        chatTextPane.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jScrollPane2.setViewportView(chatTextPane);
+
+        logLabel.setFont(new java.awt.Font("Tahoma", 2, 14)); // NOI18N
+        logLabel.setForeground(java.awt.Color.red);
+        logLabel.setText(" ");
+
+        messageTextField.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        messageTextField.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                messageTextFieldKeyPressed(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel3.setText("Invia Messaggio");
+
+        sendButton.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        sendButton.setText("SEND");
+        sendButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sendButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout chatPanelLayout = new javax.swing.GroupLayout(chatPanel);
+        chatPanel.setLayout(chatPanelLayout);
+        chatPanelLayout.setHorizontalGroup(
+            chatPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(chatPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(chatPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2)
+                    .addGroup(chatPanelLayout.createSequentialGroup()
+                        .addGroup(chatPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(chatPanelLayout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(logLabel))
+                            .addComponent(jLabel3))
+                        .addGap(0, 483, Short.MAX_VALUE))
+                    .addGroup(chatPanelLayout.createSequentialGroup()
+                        .addComponent(messageTextField)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(sendButton, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
+        chatPanelLayout.setVerticalGroup(
+            chatPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(chatPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(chatPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(logLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 361, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(chatPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(messageTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(sendButton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
+        mainPanel.setLayout(mainPanelLayout);
+        mainPanelLayout.setHorizontalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(mainPanelLayout.createSequentialGroup()
+                .addComponent(clientsPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(chatPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        mainPanelLayout.setVerticalGroup(
+            mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(clientsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(chatPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        impostazioniMenu.setText("Impostazioni");
+
+        addrCheckBox.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        addrCheckBox.setSelected(true);
+        addrCheckBox.setText("Mostra l'address A TUTTI GLI HOST");
+        addrCheckBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addrCheckBoxActionPerformed(evt);
+            }
+        });
+        impostazioniMenu.add(addrCheckBox);
+
+        jMenuBar1.add(impostazioniMenu);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(mainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    private void banButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_banButtonActionPerformed
+        String hostname= clientsList.getSelectedValue().split(" ")[0];
+        server.banna(hostname);
+    }//GEN-LAST:event_banButtonActionPerformed
+    private void clientsListValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_clientsListValueChanged
+        int index= clientsList.getSelectedIndex();
+        banButton.setEnabled(false);
+        if(index != -1) banButton.setEnabled(true);
+    }//GEN-LAST:event_clientsListValueChanged
+    private void messageTextFieldKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_messageTextFieldKeyPressed
+        if(evt.getKeyCode() == java.awt.event.KeyEvent.VK_ENTER) sendButtonActionPerformed(null);
+    }//GEN-LAST:event_messageTextFieldKeyPressed
+    private void sendButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendButtonActionPerformed
+        String message= messageTextField.getText();
+        if(message.trim().length() > 0) server.sendMessageAll(MyServer.KW_S, message);
+        messageTextField.setText("");
+        new Thread(){
+            @Override
+            public void run(){
+                sendButton.setEnabled(false);
+                try{
+                    sleep(500);
+                }catch(InterruptedException ex){}
+                if(messageTextField.isEnabled()) sendButton.setEnabled(true);
+            }
+        }.start();
+    }//GEN-LAST:event_sendButtonActionPerformed
+    private void addrCheckBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addrCheckBoxActionPerformed
+        server.setShowAddress(addrCheckBox.isSelected());
+        reloadMessages();
+    }//GEN-LAST:event_addrCheckBoxActionPerformed
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBoxMenuItem addrCheckBox;
+    private javax.swing.JButton banButton;
+    private javax.swing.JPanel chatPanel;
+    private javax.swing.JTextPane chatTextPane;
+    private javax.swing.JList<String> clientsList;
+    private javax.swing.JPanel clientsPanel;
+    private javax.swing.JMenu impostazioniMenu;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel logLabel;
+    private javax.swing.JPanel mainPanel;
+    private javax.swing.JTextField messageTextField;
+    private javax.swing.JButton sendButton;
+    // End of variables declaration//GEN-END:variables
+
+}
---
+++ aggiornati 1.1/JServer/src/jserver/MyServer.java
@@ -0,0 +1,268 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Stefano
+ */
+public class MyServer implements Runnable{
+    
+    public static final String KW_G= "global", KW_S= "SERVER"; //key Words
+    public static final String M_M= "message:" /*writer, recipient, time, text*/, M_Cl= "client:"/*name, address, state*/, M_Pass= "password:", M_Host= "hostname:"; //Messages
+    public static final String S_Conn= "connected", S_Dis= "disconnected", S_Ban= "banned"; //States
+    public static final String R_Host= "request_hostname", R_Pass= "request_password", R_Acc= "request_accepted", R_Deny= "request_denied"; //Requests
+    public static final String C_Client= "JClient:closed_connection:", C_Server= "JServer:closed_connection:"; //Closed
+    public static final String Sep= "&"; //Separator
+    
+    private ServerSocket Server;
+    private ArrayList<Connection> connessioni;
+    private ArrayList<String[]> messaggi;
+    private MainInterface interf;
+    boolean shAddr;
+
+    public MyServer() throws Exception {
+        Server = new ServerSocket(4000);
+        connessioni= new ArrayList();
+        messaggi= new ArrayList();
+        shAddr= true;
+        interf= new MainInterface(this);
+        sendMessageAll("", "IL SERVER E' IN ATTESA SULLA PORTA '4000'");
+        new Thread(this).start();
+    }
+
+    public void run() {
+        while(true){
+            try {
+                Socket client = Server.accept();
+                String hostname= client.getInetAddress().toString();
+                Connection conn= getConnectionByAddress(hostname);
+                if(conn != null) if(conn.hostname != null) hostname= conn.hostname;
+                sendMessageAll("", "Connessione accettata da: " + hostname);
+                new Thread(new Connection(client)).start();
+            } catch(IOException ex){}
+        }
+    }
+    
+    public void banna(String hostname){
+        Connection conn= getConnectionByHostname(hostname);
+        conn.bannato= true;
+        conn.closeAll("E' STATO ORA BANNATO");
+    }
+    
+    private boolean isHostnameEsistente(String hostname){
+        for(Connection conn : connessioni) if(hostname.equalsIgnoreCase(conn.hostname)) return true;
+        return false;
+    }
+    
+    private Connection getConnectionByAddress(String address){
+        for(Connection conn : connessioni) if(address.equals(conn.address)) return conn;
+        return null;
+    }
+    
+    private Connection getConnectionByHostname(String hostname){
+        for(Connection conn : connessioni) if(hostname.equals(conn.hostname)) return conn;
+        return null;
+    }
+    
+    private String[] parseClient(Connection conn){
+        String address= "", state= S_Conn;
+        if(shAddr) address= conn.address;
+        if(conn.bannato) state= S_Ban;
+        else if(conn.client.isClosed()) state= S_Dis;
+        return new String[]{conn.hostname, address, state};
+    }
+    /*
+    private void sendAll(String message){
+        for(Connection conn : connessioni) conn.send(message);
+    }*/
+    
+    public void sendMessageAll(String writer, String text){
+        long time= System.currentTimeMillis();
+        String[] message= new String[]{writer, KW_G, String.valueOf(time), text};
+        /*sendAll(M_M + writer + Sep + KW_G + Sep + time + Sep + text);*/
+        for(Connection conn : connessioni) conn.sendMessage(writer, KW_G, text);
+        messaggi.add(message);
+        interf.addMessage(message);
+    }
+    
+    private void sendClientAll(Connection conn){
+        for(Connection other : connessioni) other.sendClient(conn);
+    }
+    
+    public String[][] getMessaggi() {
+        String[][] messaggi= new String[this.messaggi.size()][];
+        for(int i= 0; i < this.messaggi.size(); i++){
+            messaggi[i]= this.messaggi.get(i);
+        }
+        return messaggi;
+    }
+    
+    public String[][] getClients(){
+        String[][] clients= new String[connessioni.size()][];
+        for(int i= 0; i < connessioni.size(); i++){
+            clients[i]= parseClient(connessioni.get(i));
+        }
+        return clients;
+    }
+    
+    public String[] getClientByName(String hostname){
+        Connection conn= getConnectionByHostname(hostname);
+        if(conn == null) return null;
+        return parseClient(conn);
+    }
+
+    public void setShowAddress(boolean show) {
+        this.shAddr = show;
+        for(Connection conn : connessioni) conn.sendAllClient();
+    }
+    
+
+    class Connection implements Runnable {
+
+        private Socket client = null;
+        private BufferedReader in = null;
+        private PrintStream out = null;
+        private String address, hostname, password;
+        private boolean bannato;
+        
+        public Connection(Socket clienSocket){
+            client = clienSocket;
+            address= client.getInetAddress().toString();
+            bannato= false;
+        }
+
+        private void initConnection() throws IOException{
+            in= new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out= new PrintStream(client.getOutputStream(), true);
+            Connection conn= getConnectionByAddress(address);
+            
+            if(conn == null) requestHostname();
+            else{
+                hostname= conn.hostname;
+                password= conn.password;
+                bannato= conn.bannato;
+                if(bannato) closeAll("E' BANNATO");
+                else{
+                    if(hostname == null) requestHostname();
+                    if(password != null) requestPassword();
+                    if(!conn.client.isClosed()) conn.closeAll("CONNESSO DA UNA NUOVA CONNESSIONE");
+                    connessioni.remove(conn);
+                }
+            }
+            if(!bannato){
+                sendAllClient();
+                connessioni.add(this);
+                sendClientAll(this);
+                interf.reloadClients();
+                if(conn == null) sendMessageAll("", "BENVENUTO " + hostname.toUpperCase() + "!");
+                else sendMessageAll("", hostname.toUpperCase() + " SI E' RICONNESSO");
+            }
+        }
+        
+        public void run(){
+            String messaggio;
+            try{
+                initConnection();
+                do{
+                    messaggio= in.readLine();
+                    if(messaggio.startsWith(M_M)) writeMessage(messaggio);
+                    else if(messaggio.startsWith(M_Pass)) password= messaggio.substring(M_Pass.length());
+                }while(!messaggio.startsWith(C_Client));
+            }catch(IOException ex){}catch(Exception ex){}
+            closeAll("E' USCITO");
+        }
+        
+        private void requestHostname() throws IOException{
+            String messaggio;
+            boolean isValido= false;
+            send(R_Host);
+            do{
+                messaggio= in.readLine();
+                if(messaggio.startsWith(M_Host)) isValido= !isHostnameEsistente(messaggio.substring(M_Host.length()));
+                if(isValido) send(R_Acc);
+                else send(R_Deny);
+            }while(!isValido);
+            hostname= messaggio.substring(M_Host.length());
+        }
+        
+        private void requestPassword() throws IOException{
+            String messaggio;
+            boolean isValido= false;
+            send(R_Pass);
+            do{
+                messaggio= in.readLine();
+                if(messaggio.startsWith(M_Pass)) isValido= (messaggio.substring(M_Pass.length()).equals(password));
+                if(isValido) send(R_Acc);
+                else send(R_Deny);
+            }while(!isValido);
+            password= messaggio.substring(M_Pass.length());
+        }
+        
+        private void writeMessage(String message){
+            message= message.substring(M_M.length());
+            String[] m= message.split(Sep);
+            String recipient= m[0];
+            String text= m[1];
+            if(recipient.equals(KW_G)) sendMessageAll(hostname, text);
+            else getConnectionByHostname(recipient).sendMessage(hostname, recipient, text);
+        }
+        
+        private void send(String message){
+            if(!client.isClosed()){
+                out.println(message);
+                out.flush();
+            }
+        }
+        
+        private void sendMessage(String writer, String recipient, String text){
+            if(!client.isClosed()){
+                long time= System.currentTimeMillis();
+                send(M_M + writer + Sep + recipient + Sep + time + Sep + text);
+            }
+        }
+        
+        private void sendClient(Connection conn){
+            if(!client.isClosed()){
+                String[] client= parseClient(conn);
+                send(M_Cl + client[0] + Sep + client[1] + Sep + client[2]);
+            }
+        }
+        
+        private void sendAllClient(){
+            if(!client.isClosed()){
+                for(Connection conn : connessioni) sendClient(conn);
+            }
+        }
+        
+        private void closeAll(String messaggio){
+            if(!client.isClosed()){
+                try{
+                    sendMessage("", "", messaggio);
+                    out.print(C_Server + messaggio);
+                    client.close();
+                    out.close();
+                    in.close();
+                }catch(IOException ex){}
+                
+                String hostname= client.getInetAddress().toString();
+                if(this.hostname != null) hostname= this.hostname;
+                sendMessageAll("", hostname.toUpperCase() + " SI E' DISCONNESSO: " + messaggio.toUpperCase());
+                sendClientAll(this);
+                interf.reloadClients();
+            }
+        }
+
+    }
+}
